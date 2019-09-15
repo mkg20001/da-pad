65bde4c88ea11285d7c6d224f6af18bd32ee3af7
Maciej Krüger <mkg20001@gmail.com>
2019-09-16 01:38:52 +0200

    feat: crdt stuff

---
+++ src/crdt.js
@@ -0,0 +1,68 @@
+'use strict'
+
+/*
+
+State looks like this:
+  {author: 'name', line: '', beforeLine: '', afterLine: '', text: '', beforeText: '', afterText: ''}
+
+*/
+
+/* const DocumentWithAuthors = {
+  initial: () => {
+    return {
+      lines: {},
+      lineOrder: []
+    }
+  },
+  join: (s1, s2) => {
+
+  },
+  value: (state) => state,
+  mutators: {
+    doSomething (id, state, arg1) => {
+      // example mutator, returning a delta
+      return 0
+    }
+  }
+}  */
+
+/*
+
+Document {
+  lineIds: RGA(),
+  lines: {
+    $id: RGA(text)
+  }
+}
+
+Text {
+  author "authorId"
+  content "someString"
+}
+
+*/
+
+const CRDT = require('delta-crdts')
+
+module.exports = (padId, authorId) => {
+  const rga = CRDT('rga')
+
+  /* const DocumentWithAuthors = {
+    initial: () => {
+      return {
+        lineIds: rga(padId + '#')
+      }
+    },
+    join: (s1, s2) => {
+
+    },
+    value: (state) => state,
+    mutators: {
+      appendLine (beforeLine) => {
+
+      }
+    }
+  } */
+
+  return rga(padId)
+}
