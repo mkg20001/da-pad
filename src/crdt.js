11e3892107db4707ef89330a0572925a6ffa4a1e
Maciej Krüger <mkg20001@gmail.com>
2019-09-16 04:36:32 +0200

    feat: finish crdt

--- src/crdt.js
+++ src/crdt.js
@@ -1,6 +1,10 @@
 'use strict'
 
 const CRDT = require('delta-crdts')
+const rga = require('delta-crdts/src/rga')
+
+const crypto = require('crypto')
+const genLineId = () => crypto.randomBytes(10).toString('hex')
 
 module.exports = () => {
   /*
@@ -16,8 +20,6 @@ module.exports = () => {
 
   */
 
-  const rga = require('delta-crdts/src/rga')
-
   function merge2rga (s1, s2) {
     if (s1 && s2) {
       return rga.join(s1, s2)
@@ -30,18 +32,28 @@ module.exports = () => {
     initial: () => {
       return {
         lines: rga.initial(),
-        lineIds: {}
+        lineIds: {},
+        delLineIds: []
       }
     },
     join: (s1, s2) => {
       let sn = {
         lines: merge2rga(s1.lines, s2.lines),
-        lineIds: {}
+        lineIds: {},
+        delLineIds: []
       }
+
+      const del = s1.delLineIds.concat(s2.delLineIds)
 
       for (const id in s1.lineIds) { // eslint-disable-line guard-for-in
         sn.lineIds[id] = merge2rga(s1.lineIds[id], s2.lineIds[id])
       }
+
+      del.forEach(id => {
+        delete sn.lineIds[id]
+      })
+
+      return sn
     },
     value: (state) => {
       return rga.value(state.lines).map(lineId => {
@@ -49,14 +61,46 @@ module.exports = () => {
       })
     },
     mutators: {
-      createLineAfter (prevLine) {
-
+      createLineAfter (id, state, after) {
+        const lid = genLineId()
+        return {
+          lines: rga.addRight(id, state.lines, after, lid),
+          lineIds: {
+            [lid]: rga.initial()
+          },
+          delLineIds: []
+        }
       },
-      deleteLineAt (id) {
+      deleteLineAt (id, state, at) {
+        const lid = rga.value(state.lines)[at]
 
+        return {
+          lines: rga.deleteAt(id, state.lines, at),
+          lineIds: {},
+          delLineIds: [lid]
+        }
       },
-      appendLine (lineId, textId, state) {
+      appendTextAfterText (id, state, lineId, afterTextId, content) {
+        const lid = rga.value(state.lines)[lineId]
 
+        return {
+          lines: rga.initial(),
+          lineIds: {
+            [lid]: rga.pushRight(id, state.lineIds[lineId], afterTextId, content)
+          },
+          delLineIds: []
+        }
+      },
+      deleteTextAt (id, state, lineId, textId) {
+        const lid = rga.value(state.lines)[lineId]
+
+        return {
+          lines: rga.initial(),
+          lineIds: {
+            [lid]: rga.deleteAt(id, state.lineIds[lineId], textId)
+          },
+          delLineIds: []
+        }
       }
     }
   }
